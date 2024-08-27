6289916f757a3ca23f0675825312511ac4b9d8f5
jerenteria <renteria.juan96@gmail.com>
2024-08-26 19:10:54 -0700

    added first route to web app

--- using_react_router/src/App.js
+++ using_react_router/src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./routes/Home";
+
+const router = createBrowserRouter([
+  // first is route path(url) we are using and second is the element which is the component that you want rendered at that path
+  { path: "/", element: <HomePage /> },
+  {}
+  ]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
