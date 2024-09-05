e3a9cf07ce70a0304056bb7d74e2ba17f4d67e87
jerenteria <renteria.juan96@gmail.com>
2024-09-04 18:45:03 -0700

    creating navigation

---
+++ using_react_router/src/routes/MainNavigation.js
@@ -0,0 +1,15 @@
+import Link from 'react-router-dom';
+
+function mainNavigation() {
+    return(
+        <nav>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/products'>Products</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+
+export default mainNavigation;
