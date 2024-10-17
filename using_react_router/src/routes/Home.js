4ebecd9d90a61735c4859f6edc39835ff694695a
jerenteria <renteria.juan96@gmail.com>
2024-10-16 18:58:55 -0700

    learned how to navigate programatically

--- using_react_router/src/routes/Home.js
+++ using_react_router/src/routes/Home.js
@@ -1,13 +1,20 @@
-import { Link } from "react-router-dom"; // Link allows to to navigate through diff pages without refreshing whole application(would take too long)
+import { Link, useNavigate } from "react-router-dom"; // Link allows to to navigate through diff pages without refreshing whole application(would take too long)
 import MainNavigation from "../components/MainNavigation";
 
 function HomePage() {
+    const navigate = useNavigate();
+
+    function navigateHandler() {
+        navigate('/products')
+    };
+
   return (
     <>
       <h1>Hello</h1>
       <p>
         Go to <Link to="/products">products</Link>
       </p>
+      <p><button onClick={navigateHandler}>navigate</button></p>
     </>
   );
 }
