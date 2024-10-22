5ea1712cd96a753be1443da990d342fa6bd3fb49
jerenteria <renteria.juan96@gmail.com>
2024-10-21 18:43:56 -0700

    added dynamic route for products

--- using_react_router/src/routes/Product.js
+++ using_react_router/src/routes/Product.js
@@ -1,11 +1,26 @@
+// using Link instead of NavLink bc we dont want to highlight those links as being active/non-active bc werea actually leaving the page when we click the links
+import { Link } from "react-router-dom";
+
+// this how we would get products from a backend and be able to render them easier on the front end. easier than hard coding
+// all the product links one by one
+const PRODUCTS = [
+  { id: "p1", title: "Product 1" },
+  { id: "p2", title: "Product 2" },
+  { id: "p3", title: "Product 3" },
+];
+
 function ProductPage() {
   return (
     <>
       <h1>Second Page</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <ul>
+          {PRODUCTS.map((prod) => (
+            <li key={prod.id}>
+              <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+            </li>
+          ))}
+        </ul>
       </ul>
     </>
   );
